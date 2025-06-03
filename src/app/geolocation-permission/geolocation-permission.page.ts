46a33956cba6660b9cf1403704b81c403fadf83e
Lenny Amstutz <lennyamstutz@gmail.com>
2025-06-03 15:20:45 +0200

    designe

--- src/app/geolocation-permission/geolocation-permission.page.ts
+++ src/app/geolocation-permission/geolocation-permission.page.ts
@@ -1,26 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {IonButton, IonContent, IonHeader, IonTitle, IonToolbar} from '@ionic/angular/standalone';
-import {Router} from "@angular/router";
-import {Geolocation} from "@capacitor/geolocation";
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-geolocation-permission',
   templateUrl: './geolocation-permission.page.html',
   styleUrls: ['./geolocation-permission.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonButton,
+  ],
 })
-export class GeolocationPermissionPage{
+export class GeolocationPermissionPage {
   constructor(private router: Router) {}
 
   async requestGeolocationPermission() {
     const result = await Geolocation.requestPermissions();
-    if (result.location === "granted") {
+    if (result.location === 'granted') {
       this.router.navigate(['/geolocation']);
     } else {
-    this.router.navigate(['/home']);
+      this.router.navigate(['/home']);
     }
   }
 }
