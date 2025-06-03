e93cad68144e57e62fc99412233e603021ed0741
Lenny Amstutz <lennyamstutz@gmail.com>
2025-06-03 14:30:01 +0200

    version update

--- src/app/camera-permission/camera-permission.page.ts
+++ src/app/camera-permission/camera-permission.page.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {IonButton, IonContent, IonHeader, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
-import {Camera} from '@capacitor/camera';
+import { Camera } from '@capacitor/camera';
 
 @Component({
   selector: 'app-camera-permission',
   templateUrl: './camera-permission.page.html',
   styleUrls: ['./camera-permission.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonButton,
+  ],
 })
-export class CameraPermissionPage{
-
+export class CameraPermissionPage {
   constructor(private router: Router) {}
-
 
   async requestCameraPermission() {
     const result = await Camera.requestPermissions();
